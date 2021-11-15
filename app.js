fcf49b2e85ce445e49a70ad856fda8f66eef081e
Lettuce Lu <lettuce.lu@lettucelu.com>
2021-11-15 21:43:21 +0800

    modify express-handlebars version

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars') // 引入 handlebars
 const app = express()
 const port = 3000
 
 // 設定 view engine 使用 handlebars
-app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.get('/', (req, res) => {
     // res.statusCode = 200;
